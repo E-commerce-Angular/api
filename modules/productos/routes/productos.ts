44a0a29ed07aaf23fae767e6ee0c8b7089618e94
Luis Parada <luisparadawagner@gmail.com>
2021-08-19 13:29:51 -0300

    Se agrega conexion a mongo

--- modules/productos/routes/productos.ts
+++ modules/productos/routes/productos.ts
@@ -3,14 +3,15 @@ import * as productosSchema from "../schemas/productos";
 
 const router = express.Router();
 
-router.get("/pepe", (request, response) => {
+router.get("/pepe", async (request, response) => {
     try {
         console.log("Entra a productos nuevos!!!");
-        let productos = productosSchema.productos.find();
+        let productos = await productosSchema.productos.find();
 
         console.log("Productos!!!", productos);
         return response.status(200).send({ status: "success", data: productos });
     } catch (err) {
+        console.log("Error: ", err)
         return response.status(404).send({ status: "error", data: err });
     }
 });
