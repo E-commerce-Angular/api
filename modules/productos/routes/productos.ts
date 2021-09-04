367c6cb4fa8bd85321212ff3f6e2200db4510a30
D-Perez85 <damianperez22@hotmail.com>
2021-09-04 04:16:54 -0300

    Agregado de verbo PUT

--- modules/productos/routes/productos.ts
+++ modules/productos/routes/productos.ts
@@ -4,23 +4,40 @@ import * as productosSchema from "../schemas/productos";
 const router = express.Router();
 
 router.post("/producto", async (req, res) => {
-    try {
-      const newProduct = req.body;
-      const productos = new productosSchema.productos(newProduct);
-      const productoNuevo = await productos.save();
-      console.log("Producto agregado", productoNuevo);
-      return res.status(200).send({ status: "success", data: productoNuevo });
-    } catch (err) {
-      console.log("Error: ", err);
-      return res.status(404).send({ status: "error", data: err });
-    }
-  });
+  try {
+    const newProduct = req.body;
+    const productos = new productosSchema.productos(newProduct);
+    const productoNuevo = await productos.save();
+    console.log("Producto agregado", productoNuevo);
+    return res.status(200).send({ status: "success", data: productoNuevo });
+  } catch (err) {
+    console.log("Error: ", err);
+    return res.status(404).send({ status: "error", data: err });
+  }
+});
 
 router.get("/productos", async (req, res) => {
   try {
     let productos = await productosSchema.productos.find();
     console.log("Productos registrados!!!", productos);
     return res.status(200).send({ status: "success", data: productos });
+  } catch (err) {
+    console.log("Error: ", err);
+    return res.status(404).send({ status: "error", data: err });
+  }
+});
+
+router.put("/producto/:id", async (req, res) => {
+  try {
+    const productUpdate = req.body;
+    const idProduct = req.params.id;
+    const productUpdated = await productosSchema.productos.findByIdAndUpdate(
+      idProduct,
+      productUpdate,
+      { new: true }
+    );
+    console.log("Producto modificado", productUpdated);
+    return res.status(200).send({ status: "success", data: productUpdated });
   } catch (err) {
     console.log("Error: ", err);
     return res.status(404).send({ status: "error", data: err });
