89ad705453ad1aa0c1c4b358ef5252b80304fd04
D-Perez85 <damianperez22@hotmail.com>
2021-09-04 02:26:46 -0300

    Agregado de verbo PUT

--- modules/usuarios/routes/usuarios.ts
+++ modules/usuarios/routes/usuarios.ts
@@ -27,6 +27,20 @@ router.get("/usuarios", async (req, res) => {
   }
 });
 
+router.put("/usuario/:id", async (req, res)=>{
+  try{
+      console.log("Id Usuario", req.params)
+      const userUpdate = req.body; 
+      const idUser = req.params.id
+      const userUpdated = await usuariosSchema.usuarios.findByIdAndUpdate(idUser, userUpdate, {new: true})
+      console.log("User modificado", userUpdated)
+      return res.status(200).send({ status: "success", data: userUpdated });
+  }catch(err){
+      console.log("Error: ", err);
+      return res.status(404).send({ status: "error", data: err });
+  }
+})
+
 
 
 export = router;
