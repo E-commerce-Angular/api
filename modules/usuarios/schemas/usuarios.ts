f79b80b5a9d23c2a72a011ddae5e0d5cfa52c15a
Merkel Gonzalo <merkelgonzalo.ok@gmail.com>
2021-09-08 22:21:41 -0300

    Agregado de password en schema de Usuario

--- modules/usuarios/schemas/usuarios.ts
+++ modules/usuarios/schemas/usuarios.ts
@@ -3,7 +3,8 @@ import * as mongoose from "mongoose";
 export const UsuarioSchema = new mongoose.Schema({
     nombreUsuario: { type: String, required: true, lowercase: true },
     apellidoUsuario: { type: String, required: true, lowercase: true },
-    dni: Number
+    dni: Number,
+    password: {type: String, required: true}
 });
 
 export let usuarios = mongoose.model("Usuario", UsuarioSchema, "usuarios");
